095e3fd7bc7995870ddf37994ff8c7411e5525ab
yuanyan3060 <1846865993@qq.com>
2025-05-01 02:42:06 +0000

    auto update

--- gamedata/[uc]lua/hotfixes/abilitystandardhotfixer.lua
+++ gamedata/[uc]lua/hotfixes/abilitystandardhotfixer.lua
@@ -1,28 +1,24 @@
 
-local eutil = CS.Torappu.Lua.Util
-
-
 local AbilityStandardHotfixer = Class("AbilityStandardHotfixer", HotfixBase)
 
-local function _OnCastEnd(self, reason)
-  self:OnCastEnd(reason)
-  if (string.find(self.owner.name, "char_2024_chyue")) then
-    self.spellCnt = 0
+local function _Fix_OnAttackFinished(self, arg)
+  if arg == nil then
+    self:OnAttackFinished(self)
+  else
+    self:OnAttackFinished(arg)
   end
 end
 
 function AbilityStandardHotfixer:OnInit()
   xlua.private_accessible(CS.Torappu.Battle.AbilityStandard)
-	
-  self:Fix_ex(CS.Torappu.Battle.AbilityStandard, "OnCastEnd", function ( self, reason )
-    local ok, errorInfo = xpcall(_OnCastEnd, debug.traceback, self, reason)
-    if not ok then
-      eutil.LogError("[AbilityStandardHotfixer] OnCastEnd fix" .. errorInfo)
-    end
-  end)
-end
 
-function AbilityStandardHotfixer:OnDispose()
+  self:Fix_ex(CS.Torappu.Battle.AbilityStandard, "OnAttackFinished",
+  function(self, arg)
+    local ok, errorInfo = xpcall(_Fix_OnAttackFinished, debug.traceback, self, arg)
+      if not ok then
+        LogError("fix AbilityStandard OnAttackFinished error" .. errorInfo)
+      end
+  end)
 end
 
 return AbilityStandardHotfixer
